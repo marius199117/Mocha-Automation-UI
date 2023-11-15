db0d9fc76f5dfa46b34f1d2a04836042f157e966
Cuibus Marius <marius199117@gmail.com>
2023-11-15 16:04:29 +0200

    temporary

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
-      // Additional setupNodeEvents logic if needed
     },
-  },
-  reporter: 'mochawesome', // Set the Mochawesome reporter
-  reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome', // Set the directory for Mochawesome reports
-    overwrite: false, // Set to true if you want to overwrite existing reports
-    html: false, // Set to true if you want HTML reports alongside the JSON files
-    json: true, // Set to true to generate JSON reports
   },
 });
